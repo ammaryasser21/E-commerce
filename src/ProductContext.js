a44a45ce2d8df61ade6e78588dadce35c4fd8a94
ammaryasser21 <ammaryasserqwertyu@gmail.com>
2024-05-12 23:45:55 +0300

    finished the project

--- src/ProductContext.js
+++ src/ProductContext.js
@@ -1,3 +1,53 @@
+// import React, { createContext, useContext, useState, useEffect } from 'react';
+
+// const ProductContext = createContext();
+
+// export const useProducts = () => {
+//   return useContext(ProductContext);
+// };
+
+// export const ProductProvider = ({ children }) => {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       try {
+//         const response = await fetch('http://localhost:5000/api/products/',{
+//           method:'GET'
+//         });
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch products');
+//         }
+//         const responseData = await response.json();
+//         const productsData = responseData.data.Products;
+//         setProducts(productsData);
+//       } catch (error) {
+//         console.error('Error fetching products:', error);
+//       }
+      
+//     };
+    
+//   fetchProducts();
+    
+//   }, []);
+
+//   const onAddReview = (productId, newReview) => {
+//     const updatedProducts = products.map(product => {
+//       if (product._id === productId) {
+//         return { ...product, reviews: [...product.reviews, newReview] };
+//       }
+//       return product;
+//     });
+//     setProducts(updatedProducts);
+//   };
+
+//   return (
+//     <ProductContext.Provider value={{ products, setProducts, onAddReview }}>
+//       {children}
+//     </ProductContext.Provider>
+//   );
+// };
+
 import React, { createContext, useContext, useState } from 'react';
 import product1 from './assets/product1.jfif';
 import Product2 from './assets/Product2.jfif';
